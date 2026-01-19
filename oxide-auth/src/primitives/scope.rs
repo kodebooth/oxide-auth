2c785a2713686b61b2323de85f5acaa69e9191af
Abe Kohandel <abe@kodebooth.com>
2026-01-19 15:37:05 -0800

    clippy(oxide-auth): resolve manual_range_contains
    Resolve the manual_range_contains clippy warning. However, a range
    pattern can be used instead of the suggestion by clippy.
    
    The warning addressed by this change can be seen below:
    
    warning: manual `RangeInclusive::contains` implementation
      --> oxide-auth/src/primitives/scope.rs:76:19
       |
    76 |             ch if ch >= '\x23' && ch <= '\x5b' => false,
       |                   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^ help: use: `('\x23'..='\x5b').contains(&ch)`
       |
       = help: for further information visit https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#manual_range_contains
       = note: `#[warn(clippy::manual_range_contains)]` on by default
    
    Link: https://rust-lang.github.io/rust-clippy/rust-1.92.0/index.html#manual_range_contains
    Signed-off-by: Abe Kohandel <abe@kodebooth.com>

--- oxide-auth/src/primitives/scope.rs
+++ oxide-auth/src/primitives/scope.rs
@@ -73,8 +73,8 @@ impl Scope {
     fn invalid_scope_char(ch: char) -> bool {
         match ch {
             '\x21' => false,
-            ch if ch >= '\x23' && ch <= '\x5b' => false,
-            ch if ch >= '\x5d' && ch <= '\x7e' => false,
+            '\x23'..='\x5b' => false,
+            '\x5d'..='\x7e' => false,
             ' ' => false, // Space seperator is a valid char
             _ => true,
         }
